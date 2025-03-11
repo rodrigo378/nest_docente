837f19266c774f8d88fffbff358bf1dfec312ac8
VictorSinche <victor.sinche@uma.edu.pe>
2025-03-11 17:56:28 -0500

    cors para habilitar el puerto de angular

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
