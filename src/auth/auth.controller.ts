67cbe6ffd574fc2c88ed5634f32f446d63ac641f
VictorSinche <victor.sinche@uma.edu.pe>
2025-03-11 17:40:44 -0500

    redirigir al front (angular)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signupDto';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { AuthenticatedUser } from 'src/types/AutenticatedUserGoogle.interface';
 import { SigninDto } from './dto/signinDto';
 
@@ -18,20 +18,23 @@ export class AuthController {
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req: Request) {
-    const user = req.user as { user: AuthenticatedUser; accessToken: string };
+  googleAuthRedirect(@Req() req: Request, @Res() res: Response) {
+    const user = req.user as { accessToken: string };
 
-    return {
-      message: 'Inicio de sesión exitoso',
-      user: {
-        id: user.user.id,
-        email: user.user.email,
-        authProvider: user.user.authProvider,
-        createdAt: user.user.createdAt,
-        updatedAt: user.user.updatedAt,
-      },
-      accessToken: user.accessToken,
-    };
+    // 🔹 Redirigir al frontend con el token en la URL
+    return res.redirect(`http://localhost:4200/login?token=${user.accessToken}`);
+
+    // return {
+    //   message: 'Inicio de sesión exitoso',
+    //   user: {
+    //     id: user.user.id,
+    //     email: user.user.email,
+    //     authProvider: user.user.authProvider,
+    //     createdAt: user.user.createdAt,
+    //     updatedAt: user.user.updatedAt,
+    //   },
+    //   accessToken: user.accessToken,
+    // };
   }
 
   @Post('signup')
