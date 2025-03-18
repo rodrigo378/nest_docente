7c19bcf47e488f50ac5fd1e77b378ae79e1f6617
VictorSinche <victor.sinche@uma.edu.pe>
2025-03-18 16:25:10 -0500

    consultas bd2

---
+++ src/Consultasdb2/consultasdb2.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('consultasdb2')
+export class Consultasdb2Controller {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { ExcelController } from './excel/excel.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { EspecialidadesController } from './especialidades/especialidades.controller';
 import { EspecialidadesService } from './especialidades/especialidades.service';
+import { Consultasdb2Service } from './consultasdb2/consultasdb2.service';
+import { Consultasdb2Controller } from './consultasdb2/consultasdb2.controller';
 
 @Module({
   imports: [
@@ -18,7 +20,7 @@ import { EspecialidadesService } from './especialidades/especialidades.service';
     UbicacionModule,
     DocenteModule,
   ],
-  controllers: [ExcelController, EspecialidadesController],
-  providers: [ExcelService, EspecialidadesService],
+  controllers: [ExcelController, EspecialidadesController, Consultasdb2Controller],
+  providers: [ExcelService, EspecialidadesService, Consultasdb2Service],
 })
 export class AppModule {}
