180a095081bf38977e70592b6f2be694f35c275c
RODRIGO PALOMINO CASTRO <rodrigo.palomino@umared.com>
2025-03-26 12:50:59 -0500

    getCurso con equi

--- src/sigu/sigu.service.ts
+++ src/sigu/sigu.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { PrismaReadonlyService } from 'src/prisma/readonly.service';
 import { GetCursoDto } from './dto/getCursoDto';
 
-interface CursoQuery {
-  n_codper: number;
-  c_codmod: string;
-  c_nommod: string;
-  c_codfac: string;
-  c_codesp: string;
-  n_ciclo: number;
-  c_ciclo: string;
-  c_codcur: string;
-  c_nomcur: string;
-  n_ht: number;
-  n_hp: number;
-  equivalencias: string | null;
-}
+// interface CursoQuery {
+//   n_codper: number;
+//   c_codmod: string;
+//   c_nommod: string;
+//   c_codfac: string;
+//   c_codesp: string;
+//   n_ciclo: number;
+//   c_ciclo: string;
+//   c_codcur: string;
+//   c_nomcur: string;
+//   n_ht: number;
+//   n_hp: number;
+//   equivalencias: string | null;
+// }
 
 @Injectable()
 export class SiguService {
@@ -66,80 +66,79 @@ export class SiguService {
   }
 
   async getCursos(getCursoDto: GetCursoDto) {
-    const cursos = await this.prismaReadonly.$queryRawUnsafe<CursoQuery[]>(
-      `SELECT 
+    return await this.prismaReadonly.$queryRawUnsafe(
+      `SELECT
         tp.n_codper,
         tp.c_codmod,
         tb.c_nommod,
         tp.c_codfac,
         tp.c_codesp,
+        tp.c_area,
+        tpec.c_nom_cur_area,
         tp.n_ciclo,
         tp.c_ciclo,
         tp.c_codcur,
         tp.c_nomcur,
         tp.n_ht,
         tp.n_hp,
-        GROUP_CONCAT(DISTINCT CONCAT(te.c_codcur_equ, ' - ', IFNULL(te.c_nomcur_equ, 'SIN NOMBRE')) SEPARATOR ' , ') AS equivalencias
-        -- GROUP_CONCAT(DISTINCT te.c_codcur_equ SEPARATOR ' , ') AS codigos_equivalentes,
-        -- GROUP_CONCAT(te.c_nomcur_equ SEPARATOR ' , ') AS nombres_equivalentes
+        tpee.n_codper_equ,
+        tpee.c_codmod_equ,
+        tpee.c_codfac_equ,
+        tpee.c_codesp_equ,
+        tpee.c_codcur_equ,
+        tpee.c_nomcur_equ
       FROM
-          tb_plan_estudio_curso tp
-      INNER JOIN tb_modalidad tb ON tb.c_codmod = tp.c_codmod
-      LEFT JOIN (
-          SELECT distinct
-              te.c_codcur,
-              te.c_codcur_equ,
-              tp2.c_nomcur AS c_nomcur_equ
+        tb_plan_estudio_curso tp
+        INNER JOIN tb_modalidad tb ON tb.c_codmod = tp.c_codmod 
+        INNER JOIN tb_plan_estudio_curso_area tpec ON tpec.c_cod_cur_area = tp.c_area
+        left JOIN (
+        SELECT 
+          te.n_codper_equ,
+          te.c_codmod_equ,
+          te.c_codfac_equ,
+          te.c_codesp_equ,
+          te.c_codcur_equ,
+          tp2.c_nomcur AS c_nomcur_equ
           FROM tb_plan_estudio_equ te
           INNER JOIN tb_plan_estudio_curso tp2 ON te.c_codcur_equ = tp2.c_codcur
-          WHERE te.n_codper_equ in (2023, 2025)
-      ) te ON te.c_codcur = tp.c_codcur
-      WHERE
-          tp.n_codper IN (2023 , 2025)
+          WHERE te.n_codper_equ in (2023, 2025)) tpee 
+          ON tpee.c_codcur_equ = tp.c_codcur
+          and tpee.c_codmod_equ = tp.c_codmod 
+          and tpee.c_codfac_equ = tp.c_codfac 
+          and tpee.c_codesp_equ = tp.c_codesp
+        WHERE
+        tp.n_codper IN ( 2023, 2025 ) 
           AND tp.c_codfac = ?
           AND tp.c_codesp = ?
           AND tp.n_ciclo = ?
           AND tp.c_codmod = ?
       GROUP BY
-          tp.n_codper,
-          tp.c_codmod,
-          tb.c_nommod,
-          tp.c_codfac,
-          tp.c_codesp,
-          tp.n_ciclo,
-          tp.c_ciclo,
-          tp.c_codcur,
-          tp.c_nomcur,
-          tp.n_ht,
-          tp.n_hp
-      ORDER BY tp.c_nomcur;
+        tp.n_codper,
+        tp.c_codmod,
+        tp.c_codfac,
+        tp.c_codesp,
+        tp.c_area,
+        tpec.c_nom_cur_area,
+        tp.c_codcur,
+        tp.c_nomcur,
+        tp.n_ciclo,
+        tp.c_ciclo,
+        tp.n_ht,
+        tp.n_hp,
+        tpee.n_codper_equ,
+        tpee.c_codmod_equ,
+        tpee.c_codfac_equ,
+        tpee.c_codesp_equ,
+        tpee.c_codcur_equ,
+        tpee.c_nomcur_equ
+      ORDER BY
+        tp.c_nomcur;
       `,
       getCursoDto.c_codfac,
       getCursoDto.c_codesp,
       getCursoDto.n_ciclo,
       getCursoDto.c_codmod,
     );
-
-    const cursosModificados = cursos.map((curso) => {
-      const equivalencias: string[] = curso.equivalencias
-        ? curso.equivalencias.split(',').map((e) => e.trim())
-        : [];
-
-      const equ = equivalencias.map((equivalencia) => {
-        const array = equivalencia.split('-');
-        return {
-          c_codcur_equ: array[0],
-          c_nomcur_equ: array[1],
-        };
-      });
-
-      return {
-        ...curso,
-        equivalencias: equ,
-      };
-    });
-
-    return cursosModificados;
   }
 
   // async getCursos(getCursoDto: GetCursoDto) {
